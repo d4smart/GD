25c28284bd5215261cfa5bf92adeb6a6b63b6e6a
d4smart <d4smart@foxmail.com>
2016-11-22 10:11:12 +0800

    完善图片处理类和测试用例；添加readme

--- image.class.php
+++ image.class.php
@@ -41,7 +41,7 @@ class Image
     }
 
     /**
-     * 创建图像的缩略图
+     * 生成图像的缩略图
      * @param int $width 缩略图宽度
      * @param int $height 缩略图高度
      */
@@ -56,9 +56,9 @@ class Image
      * 在图像中添加文字
      * @param string $content 文字内容
      * @param int $size 文字大小
-     * @param array $position 文字位置（x，y），如array(20, 20)
+     * @param array $position 文字位置（x，y），如array(20, 20)，左上角坐标系，以文字的左下角为基准
      * @param array $color 文字颜色（R，G，B），如array(255, 255, 255)
-     * @param int $alpha 文字透明度，0-100
+     * @param int $alpha 文字透明度，0-127，0为完全不透明
      * @param int $angle 文字旋转角度，默认为0
      * @param string $font 字体路径，默认为系统字体
      */
@@ -70,8 +70,8 @@ class Image
     /**
      * 在图像中添加图像水印
      * @param string $source 水印图象路径
-     * @param array $position 水印位置，如array(30, 20)
-     * @param int $alpha 水印透明度
+     * @param array $position 水印位置，如array(30, 20)，默认为右下角坐标系，以水印图片右下角为基准
+     * @param int $alpha 水印透明度，0-100，0为完全透明
      */
     public function imageMark($source, $position, $alpha) {
         // 创建水印图象
@@ -81,7 +81,7 @@ class Image
         $waterImage = $func($source);
 
         // 添加水印
-        imagecopymerge($this->image, $waterImage, $position[0], $position[1], 0, 0, $info[0], $info[1], $alpha);
+        imagecopymerge($this->image, $waterImage, $this->info['width'] - $position[0] - $info[0], $this->info['height'] - $position[1] - $info[1], 0, 0, $info[0], $info[1], $alpha);
         imagedestroy($waterImage);
     }
 
--- test.php
+++ test.php
@@ -7,13 +7,18 @@
  * Email:   d4smart@foxmail.com
  * Github:  https://github.com/d4smart
  */
+
 require "image.class.php";
 
-$src = 'bg3.jpg';
-$source = 'thumb1.jpeg';
-$location = array(20, 30);
-$alpha = 60;
-
-$image = new Image($src);
-$image->imageMark($source, $location, $alpha);
+// 给图片添加水印图片和文字
+$image = new Image('bg3.jpg');
+$image->imageMark('waterImage.jpg', array(0, 0), 60);
+$image->fontMark("这是醉吼的", 20, array(360, 450), array(0, 0, 0), 60);
 $image->show();
+//$image->save('output1');
+
+// 生成图片的缩略图
+//$image = new Image('qrcode.jpg');
+//$image->thumb();
+//$image->show();
+//$image->save('output2');
